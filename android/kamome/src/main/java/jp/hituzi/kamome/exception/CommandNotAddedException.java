240b41decbb8ee6c92e933456632bfb28a1213f3
Douglas Soares <soareseneves@terra.com.br>
2021-03-25 14:46:59 -0300

    iOS

--- android/kamome/src/main/java/jp/hituzi/kamome/exception/CommandNotAddedException.java
+++
@@ -1,8 +0,0 @@
-package jp.hituzi.kamome.exception;
-
-public final class CommandNotAddedException extends RuntimeException {
-
-    public CommandNotAddedException(String commandName) {
-        super(commandName + " command not added.");
-    }
-}
